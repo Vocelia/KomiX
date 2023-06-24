04d6d8a09508ff254e1f572cab5e008ea5cd67dc
Vocelia <vocelia@outlook.jp>
2023-06-24 20:05:07 +0300

    Removing all declarations

--- src/include/predefined.h
+++ src/include/predefined.h
@@ -1,23 +1,7 @@
-#include <libwebsockets.h>
+#pragma once
 
-unsigned int PORT;
-unsigned int TIMEOUT;
 //Maximum Receive Buffer is set to 8 bytes
 #define MAX_RX_BUFFER_BYTES 8
 
-struct payload {
-  size_t len;
-  unsigned char data[LWS_SEND_BUFFER_PRE_PADDING+MAX_RX_BUFFER_BYTES+LWS_SEND_BUFFER_POST_PADDING];
-} rx;
-
-const char* head = "<!DOCTYPE html> \
-<html> \
-<head> \
-<title>KomiX</title> \
-<link rel=\"stylesheet\" href=\"index.css\"> \
-<link rel=\"stylesheet\" href=\"pico.min.css\"> \
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0\"> \
-</head> \
-<body>\n";
-const char* script = "</body>\n<script>\n";
-const char* footer = "</script>\n</html>";
+extern unsigned int PORT;
+extern unsigned int TIMEOUT;
