cc3815704cd4e9c8742608ebf09dd65bffaad46f
Vocelia <vocelia@outlook.jp>
2023-06-26 17:34:01 +0300

    Added more global variables

--- src/include/predefined.h
+++ src/include/predefined.h
@@ -3,5 +3,7 @@
 //Maximum Receive Buffer is set to 8 bytes
 #define MAX_RX_BUFFER_BYTES 8
 
-extern unsigned int PORT;
-extern unsigned int TIMEOUT;
+extern unsigned PORT;
+extern unsigned TIMEOUT;
+extern const char* LOGS_PATH;
+extern unsigned char COMMAND_OUTPUT;
