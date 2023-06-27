df8b37388e6fb3f1337171408589cab9461ff8b7
Vocelia <vocelia@outlook.jp>
2023-06-27 22:07:32 +0300

    Moved static functions to misc.cpp

--- src/uigen.cpp
+++ src/uigen.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <cstdlib>
 
+#include "include/misc.h"
 #include "include/config.h"
 #include "include/predefined.h"
 #include "include/wrapper/String.h"
@@ -12,46 +13,6 @@ static const char* HEAD = "<!DOCTYPE html>\n<html>\n<head>\n<title>KomiX</title>
 static const char* STYLE = "</style>\n</head>\n<body>\n";
 static const char* SCRIPT = "</body>\n<script>\n";
 static const char* FOOTER = "</script>\n</html>";
-
-static void replace_token(char* str, char tkn, char new_tkn) {
-  unsigned int cursor = 0;
-  while(str[cursor] != '\0') {
-    if(str[cursor] == tkn) str[cursor] = new_tkn;
-    cursor++;
-  }
-}
-
-static size_t get_filesize(const char* path) {
-  FILE* file = fopen(path, "r");
-  fseek(file, 0, SEEK_END); //places the cursor at the EOF
-  size_t file_size = ftell(file); //gets size of file through EOF
-  fclose(file);
-  return file_size;
-}
-
-static char* read_file(const char* path) {
-  FILE* file = fopen(path, "r");
-  size_t buff_size = get_filesize(path);
-  char* buff = new char[buff_size];
-  size_t progress = fread(buff, sizeof(buff[0]), buff_size, file);
-  fclose(file);
-  if (progress != buff_size) {
-    printf("[ERR]: Failed to read the entire content of \"%s\"!\n", path);
-    return nullptr;
-  }
-  return buff;
-}
-
-static int write_file(const char* path, const char* buff, size_t buff_size) {
-  FILE* file = fopen(path, "w");
-  size_t progress = fwrite(buff, sizeof(buff[0]), buff_size, file);
-  fclose(file);
-  if (progress != buff_size) {
-    printf("[ERR]: Failed to write the entire content of \"%s\"!\n", path);
-    return -1;
-  }
-  return 0;
-}
 
 int create_page() {
   printf("Reading from config.ini...\n");
@@ -114,7 +75,7 @@ int create_page() {
   delete[] js_buff;
   delete[] css_buff;
   delete[] css_fw_buff;
-  if (write_file("./ui/index.html", html_dump.toCharArray(), html_dump.length())==-1) {
+  if (write_file("./ui/index.html", "w", html_dump.toCharArray(), html_dump.length())==-1) {
     printf("[ERR]: Failed to write to \"./ui/index.html\"!\n");
     return -1;
   }
