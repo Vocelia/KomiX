cb5e014fbc990d3cd330a0658e5d1c65fa85be91
Vocelia <vocelia@outlook.jp>
2023-06-28 18:33:50 +0300

    Minor addition and modification

--- src/include/predefined.h
+++ src/include/predefined.h
@@ -1,9 +1,12 @@
 #pragma once
 
-//Maximum Receive Buffer is set to 8 bytes
-#define MAX_RX_BUFFER_BYTES 8
+#include "config.h"
+
+//Maximum Receive Buffer is set to 22 bytes
+#define MAX_RX_BUFFER_BYTES 22
 
 extern unsigned PORT;
+extern Config* CONFIG;
 extern unsigned TIMEOUT;
 extern const char* LOGS_PATH;
 extern unsigned char COMMAND_OUTPUT;
