afa07b001feef3920ff3397f1992046512a0b05e
Vocelia <vocelia@outlook.jp>
2023-06-28 18:33:09 +0300

    Added additional miscellaneous functions

--- src/include/misc.h
+++ src/include/misc.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdio>
 
+bool dir_exists(const char* path);
 char* read_file(const char* path);
 size_t get_filesize(const char* path);
-int write_file(const char* path, const char* buff, size_t buff_size);
+char** strspl(const char* str, const char tkn, size_t* len);
+int write_file(const char* path, const char* mode, const char* buff, size_t buff_size);
--- src/misc.cpp
+++ src/misc.cpp
@@ -1,4 +1,50 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <windows.h>
+bool dir_exists(const char* path) {
+  DWORD attrib = GetFileAttributesA(path);
+  return (attrib != INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+#else
+#include <dirent.h>
+bool dir_exists(const char* path) {
+  DIR* dir = opendir(path);
+  if (dir) closedir(dir);
+  else if (ENOENT == errno) return false;
+  else return false;
+  return true;
+}
+#endif
+
+//str: String to be split
+//delim: Delimiter to split upon
+//len: Pointer to a length variable
+char** strspl(const char* str, const char delim, size_t* len) {
+  char** spl;
+  unsigned i;
+  size_t words = 0;
+  char* tmp = (char*)malloc(strlen(str)*sizeof(char));
+  memcpy(tmp, str, strlen(str));
+  char* bgn_tmp = tmp;
+  //Count up the words and splitting them
+  while ((tmp=strchr(tmp, delim))!=NULL) {
+	  sprintf(tmp, ""); words++; tmp++;
+  } words++;
+  tmp = bgn_tmp; //Reset offset
+  spl = (char**)calloc(1, words*sizeof(char*));
+  if (!spl) return NULL;
+  //Assign each word to spl members
+  for (i=0; i<words; i++) {
+	  if (*tmp) spl[i] = tmp;
+	  tmp = strchr(tmp, '\0'); tmp++;
+  } 
+  *len = words;
+  return spl;
+}
 
 size_t get_filesize(const char* path) {
   FILE* file = fopen(path, "r");
@@ -21,8 +67,8 @@ char* read_file(const char* path) {
   return buff;
 }
 
-int write_file(const char* path, const char* buff, size_t buff_size) {
-  FILE* file = fopen(path, "w");
+int write_file(const char* path, const char* mode, const char* buff, size_t buff_size) {
+  FILE* file = fopen(path, mode);
   size_t progress = fwrite(buff, sizeof(buff[0]), buff_size, file);
   fclose(file);
   if (progress != buff_size) {
